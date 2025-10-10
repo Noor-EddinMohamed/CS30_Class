8002e472b9ccc100500be8ce7458b349341c3c26
Noor-EddinMohamed <nooreddinmohamed@gmail.com>
2025-10-10 09:23:43 -0600

    Decided on line circle illusion

---
+++ arrays_objects/sketch.js
@@ -0,0 +1,15 @@
+// Line Circle Illusion
+// Noor-Eddin Mohamed
+// October 26 2025
+// https://www.youtube.com/shorts/iKsOTNWyum8
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+}
--- index.md
+++ index.md
@@ -4,5 +4,7 @@
 - [Traffic Lights](traffic_lights)
 - [Bouncing Balls](bouncing_balls)
 - [Generative Art](generative_art)
+
 ## Assignments
 - [Interactive Scene](interactive+scene)
+- [Arrays and Objects](arrays_objects)
